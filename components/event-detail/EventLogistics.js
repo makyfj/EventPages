b47de7581f8d6ecb1d16544b845628f0ac71d967
Maky <franklinjaradev@gmail.com>
2021-07-18 17:52:27 -0400

    Added EventDetails for a single event

---
+++ components/event-detail/EventLogistics.js
@@ -0,0 +1,32 @@
+import AddressIcon from "../icons/AddressIcon";
+import DateIcon from "../icons/DateIcon";
+import LogisticsItem from "./LogisticsItem";
+import classes from "./EventLogistics.module.css";
+import Image from "next/image";
+
+function EventLogistics({ date, address, image, imageAlt }) {
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const addressText = address.replace(", ", "\n");
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <Image src={`/${image}`} alt={imageAlt} width={300} height={300} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
+
+const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Not Event Found!</p>;
+  }
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+};
+
+export default EventDetailPage;
