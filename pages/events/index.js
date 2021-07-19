b99a5718b26aea0de72697c92986eb4c5ecdcdae
Maky <franklinjaradev@gmail.com>
2021-07-18 20:56:43 -0400

    Filtered Events Bar and router slug

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 const EventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
