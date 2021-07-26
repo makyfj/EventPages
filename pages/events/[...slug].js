ea93f13209ddfa69488c881494698ec0f8fa0066
Maky <franklinjaradev@gmail.com>
2021-07-26 14:06:28 -0400

    Using SSR for slug pages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,33 +1,25 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/EventList";
 import ResultsTitle from "../../components/results-title/ResultsTitle";
 import Button from "../../components/ui/Button";
 import ErrorAlert from "../../components/error-alert/ErrorAlert";
 
-const FilteredEventsPage = () => {
-  const router = useRouter();
-  const filterData = router.query.slug;
+const FilteredEventsPage = (props) => {
+  // const router = useRouter();
+  // const filterData = router.query.slug;
 
-  if (!filterData) {
-    return <p className="center">Loading....</p>;
-  }
+  // if (!filterData) {
+  //   return <p className="center">Loading....</p>;
+  // }
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2020 ||
-    numMonth > 12 ||
-    numMonth < 1
-  ) {
+  if (props.hasError) {
     return (
       <>
         <div className="center">
@@ -40,10 +32,7 @@ const FilteredEventsPage = () => {
     );
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+  const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -58,7 +47,7 @@ const FilteredEventsPage = () => {
     );
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  const date = new Date(props.date.year, props.date.month - 1);
 
   return (
     <>
@@ -67,5 +56,45 @@ const FilteredEventsPage = () => {
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2020 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return {
+      // if function fails, error is true
+      props: { hasError: true },
+    };
+  }
+
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  };
+}
 
 export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
-const EventsPage = () => {
+const EventsPage = (props) => {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -20,5 +20,16 @@ const EventsPage = () => {
     </>
   );
 };
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default EventsPage;
