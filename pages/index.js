f9198ad8911a0fadcb5fd9c12d39badad13e2953
Maky <franklinjaradev@gmail.com>
2021-07-26 14:27:03 -0400

    Added Head for meta

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Head from "next/head";
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../helpers/api-utils";
 
 const EventsHomePage = ({ events }) => {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name="description" content="Find great events to network" />
+      </Head>
       <h1 className="header-home">Featured Events!</h1>
       <div>
         <EventList items={events} />
